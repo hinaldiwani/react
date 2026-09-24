16718eb02771af60f1d44da0791f428084df7471
Hinal <hinal@example.com>
2026-09-24 19:37:01 +0530

    First step

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,10 @@
+
+ 
+
+function App() {
+  return (
+   <h1>React | Hinal</h1>
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+  
+
+
+function App() {
+ 
+
+  return (
+   <h1>React with vite | Hinal</h1>
+  )
+}
+
+export default App
